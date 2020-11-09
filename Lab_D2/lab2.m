0c3f91f8a431f9623b5f833fb12570f670d2e87a
Martinakraft99 <martinakraft99@gmail.com>
2020-11-09 12:45:02 +0100

    Added improved plotting and Lab3Files

--- Lab_D2/lab2.m
+++ Lab_D2/lab2.m
@@ -1,3 +1,12 @@
+%initialize
+heli = input('Chose heli (1-10): ');
+if(heli < 3)
+    init_heli_1_2
+else
+    init_heli_3_10
+end
+
+%set constants
 Vs0 = 5.48;
 
 Kf = -(l_c*g*m_c - l_h*g*2*m_p)/(l_h*Vs0);
@@ -6,36 +15,69 @@ K1 = l_p*Kf/(2*m_p*l_p^2);
 
 K2 = l_h*Kf/(m_c*l_c^2+2*m_p*l_h^2);
 
-%Syst
-A = [0 1 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 1 0 0 0 0; 0 0 1 0 0];
-%A = [0 1 0; 0 0 0; 0 0 0];
- B = [0 0; 0 K1; K2 0; 0 0; 0 0];
-%B = [0 0; 0 K1; K2 0];
+n = input('Chose task (1-3): ');
+switch(n)
+    case 1
+        disp('Task 1, Pole Placement')
+        
+        A = [0 1 0; 0 0 0; 0 0 0];
+        B = [0 0; 0 K1; K2 0];
+        
+        syms p_1 p_2 p_3;
+        
+        P = [-2; -5 - 3*1i; -5 + 3*1i];
+        
+        K = place(A,B,P)
+          
+        %Runs plotting.m
+        
+        plotting
 
-    
+    case 2
+        disp('Task 2, LQR')
 
-syms k_11 k_12 k_13 k_21 k_22 k_23;
+        A = [0 1 0; 0 0 0; 0 0 0];
+        B = [0 0; 0 K1; K2 0];
+        
+        Q = diag([50; 10; 1]);
+        
+        R = diag([.1; .5]);
 
-%K = [k_11 k_12 k_13; k_21 k_22 k_23];
+        K = lqr(A,B,Q,R)
 
-%syms p_1 p_2 p_3;
-
-%P = [-2; -5 - 3*i; -5 + 3*i];
-
-%K = place(A,B,P)
-
-% --------------------------------------- LQR
-Q = diag([50; 10; 1; 1; 5]);
-%Q = diag([50; 10; 1]);
-
-R = diag([.1; .5]);
-
-K = lqr(A,B,Q,R)
-
-k_11 = K(1,1); 
-k_13 = K(1,3); 
-k_21 = K(2,1); 
-k_23 = K(2,3); 
+        k_11 = K(1,1); 
+        k_13 = K(1,3); 
+        k_21 = K(2,1); 
+        k_23 = K(2,3); 
 
 
-F = [k_11 k_13; k_21 k_23]
+        F = [k_11 k_13; k_21 k_23]
+        
+        %Runs plotting.m
+        plotting
+
+        
+    case 3
+        disp('Task 2, LQR augmented system')
+
+        A = [0 1 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 1 0 0 0 0; 0 0 1 0 0];
+        B = [0 0; 0 K1; K2 0; 0 0; 0 0];
+        
+        Q = diag([50; 10; 1; 1; 5]);
+        
+        R = diag([.1; .5]);
+
+        K = lqr(A,B,Q,R)
+
+        k_11 = K(1,1); 
+        k_13 = K(1,3); 
+        k_21 = K(2,1); 
+        k_23 = K(2,3); 
+
+
+        F = [k_11 k_13; k_21 k_23]
+        
+        %Runs plotting.m
+        plotting
+
+end
--- Lab_D2/plotting.m
+++ Lab_D2/plotting.m
@@ -1,16 +1,176 @@
+%PARAM
+%1  -   time           [s]       
+%2  -   travel         [rad]
+%3  -   travel rate    [rad/s]
+%4  -   pitch          [rad]
+%5  -   pitch rate     [rad/s]
+%6  -   elevation      [rad]
+%7  -   elevation rate [rad/s] 
+
+switch(n)
+    case 1
+        
+        %Load files
+        %--------------------------------------
+        load('(-1,i).mat') 
+        Data1 = ans;
+
+        load('(-5,3i).mat')
+        Data2 = ans;
+
+        load('(2,-2+i,-2-i).mat')
+        Data3 = ans;
+
+        load('(-2,-5-3i,-5+3i).mat')
+        Data4 = ans;
+
+        load('(-2.5,4i).mat')
+        Data5 = ans;
+        %--------------------------------------
+        
+        lowest_common = 3648;
+
+        figure
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(4,1:lowest_common)) ...
+            ,Data(1,1:lowest_common),rad2deg(Data2(4,1:lowest_common)) ...
+            ,Data(1,1:lowest_common),rad2deg(Data3(4,1:lowest_common))  ...
+            ,Data(1,1:lowest_common),rad2deg(Data4(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data5(4,1:lowest_common)))
+        
+        title('Pitch')
+        xlabel('t')
+        ylabel('deg')
+        legend('(-1,i)','(-5,3i)','(2,-2±i)','(-2,-5±3i)','((-2.5,4i))');
+        grid on;
 
 
-load('(-1,i).mat') %Choose file to load
+        figure(2)
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data2(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data3(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data4(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data5(6,1:lowest_common)))
+        
+        title('Elevation')
+        xlabel('t')
+        ylabel('deg')
+        legend('(-1,i)','(-5,3i)','(2,-2±i)','(-2,-5±3i)','((-2.5,4i))');
+        grid on;
 
-Data = ans;     
+
+        figure(3)
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(4,1:lowest_common)) ... 
+            ,Data1(1,1:lowest_common),rad2deg(Data2(4,1:lowest_common)) ... 
+            ,Data1(1,1:lowest_common),rad2deg(Data4(4,1:lowest_common)) ... 
+            ,Data1(1,1:lowest_common),rad2deg(Data5(4,1:lowest_common)))
+        
+        title('Pitch')
+        xlabel('t')
+        ylabel('deg')
+        legend('(-1,i)','(-5,3i)','(-2,-5±3i)','((-2.5,4i))');
+        grid on;
 
 
+        figure(4)
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data2(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data4(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data5(6,1:lowest_common)))
+        
+        title('Elevation')
+        xlabel('t')
+        ylabel('deg')
+        legend('(-1,i)','(-5,3i)','(-2,-5±3i)','((-2.5,4i))');
+        grid on;
 
-figure
-plot(Data(1,:),rad2deg(Data(4,:)))
-grid on;
+        
+    case 2
+        %Load files
+        %--------------------------------------
+        load('Q(10,10,10)R(10,10).mat') 
+        Data1 = ans;
 
-figure(2)
-plot(Data(1,:),rad2deg(Data(6,:)))
-grid on;
+        load('Q(10,10,5)R(.1,.1).mat')
+        Data2 = ans;
+
+        load('Q(10,100,100)R(1,1).mat')
+        Data3 = ans;
+
+        load('Q(10,50,5)R(.1,.1).mat')
+        Data4 = ans;
+
+        load('Q(50,10,1)R(.1,.1).mat')
+        Data5 = ans;
+        %--------------------------------------
+        
+        lowest_common = 3648;
+        
+        figure
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data2(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data3(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data4(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data5(4,1:lowest_common)))
+        
+        title('Pitch')
+        xlabel('t')
+        ylabel('deg')
+        legend('Q(10,10,10)R(10,10)','Q(10,10,5)R(.1,.1)','Q(10,100,100)R(1,1)','Q(10,50,5)R(.1,.1)','Q(50,10,1)R(.1,.1)');
+        grid on;
+        
+        figure(2)
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data2(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data3(6,1:lowest_common)) ...
+            ,Data(1,1:lowest_common),rad2deg(Data4(6,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data5(6,1:lowest_common)))
+        
+        title('Elevation')
+        xlabel('t')
+        ylabel('deg')
+        legend('Q(10,10,10)R(10,10)','Q(10,10,5)R(.1,.1)','Q(10,100,100)R(1,1)','Q(10,50,5)R(.1,.1)','Q(50,10,1)R(.1,.1)');
+        grid on;
+        
+
+    case 3
+        %Load files
+        %--------------------------------------
+        load('Q(50,10,1,1,1)R(.1,.1).mat') 
+        Data1 = ans;
+
+        load('Q(50,10,1,1,10)R(.1,.1).mat')
+        Data2 = ans;
+
+        load('Q(50,10,1,1,10)R(.1,.5).mat')
+        Data3 = ans;
+
+        %--------------------------------------
+        
+        
+        lowest_common = 3648;
+        
+        figure
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data2(4,1:lowest_common)) ... 
+            ,Data(1,1:lowest_common),rad2deg(Data3(4,1:lowest_common)))
+        
+        title('Pitch')
+        xlabel('t')
+        ylabel('deg')
+        legend('Q(50,10,1,1,1)R(.1,.1)','Q(50,10,1,1,10)R(.1,.1)','Q(50,10,1,1,10)R(.1,.5)');
+        grid on;
+        
+        figure(2)
+        plot(Data1(1,1:lowest_common),rad2deg(Data1(6,1:lowest_common)) ...
+            ,Data(1,1:lowest_common),rad2deg(Data2(6,1:lowest_common)) ...
+            ,Data(1,1:lowest_common),rad2deg(Data3(6,1:lowest_common)))
+        
+        title('Elevation')
+        xlabel('t')
+        ylabel('deg')
+        legend('Q(50,10,1,1,1)R(.1,.1)','Q(50,10,1,1,10)R(.1,.1)','Q(50,10,1,1,10)R(.1,.5)');
+        grid on;
+      
+end
+    
 
