73ea3f92d120e406f10bbdc3c8ccb7c6c511c4bd
Andreas Gudahl Tufte <andregtu@stud.ntnu.no>
2020-11-14 17:32:29 +0100

    Individual lab files now in respective folder

--- Lab_D2/PDcontrol.m
+++ Lab_D2/PDcontrol.m
@@ -6,6 +6,6 @@ K1 = l_p*Kf/(2*m_p*l_p^2);
 
 
 pole1 = -5 + 3i; % we need a minimum of 1-2 in imaginary part
-%pole1 = -7 + 2i; %long settling-time on medium disturbance
+%pole1 = -7 + 2i; % long settling-time on medium disturbance
 
 pole2 = conj(pole1);
